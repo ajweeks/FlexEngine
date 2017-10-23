194fb34fd61fe273163dd78cd25b9f8ddcc947d9
AJ Weeks <ajweeks@shaw.ca>
2017-10-23 22:46:30 +0200

    Add parent-child transform functionality

--- FlexEngine/include/Transform.hpp
+++ FlexEngine/include/Transform.hpp
@@ -13,20 +13,42 @@ namespace flex
 		Transform(const glm::vec3& position, const glm::quat& rotation);
 		Transform(const glm::vec3& position);
 		~Transform();
-
-		void Translate(glm::vec3 deltaPosition);
-		void Rotate(glm::quat deltaQuatRotation);
-		void Rotate(glm::vec3 deltaEulerRotationRad);
-		void Scale(glm::vec3 deltaScale);
+		
 		void SetAsIdentity();
 
+		void Translate(glm::vec3 deltaPosition);
+		void Translate(float deltaX, float deltaY, float deltaZ);
+
+		void Rotate(glm::quat deltaQuatRotation);
+		void Rotate(glm::vec3 deltaEulerRotationRad);
+		void Rotate(float deltaX, float deltaY, float deltaZ);
+		
+		void Scale(glm::vec3 deltaScale);
+		void Scale(float deltaX, float deltaY, float deltaZ);
+
+		void SetParentTransform(Transform* parent);
+		void AddChildTransform(Transform* child);
+		void RemoveChildTransform(Transform* child);
+		void RemoveAllChildTransforms();
+		
 		glm::mat4 GetModelMatrix();
 
 		static Transform Identity();
 
-		glm::vec3 position;
-		glm::quat rotation;
-		glm::vec3 scale;
+		glm::vec3 localPosition;
+		glm::quat localRotation;
+		glm::vec3 localScale;
+
+		glm::vec3 globalPosition;
+		glm::quat globalRotation;
+		glm::vec3 globalScale;
+
+		Transform* parentTransform = nullptr;
+		std::vector<Transform*> childrenTransforms;
+
+	private:
+		void UpdateParentTransform(); // Used to go all the way to the base of the parent-child tree
+		void UpdateChildTransforms(); // Used to go back down to the lowest node of the parent-child tree
 
 	};
 } // namespace flex
--- FlexEngine/src/Transform.cpp
+++ FlexEngine/src/Transform.cpp
@@ -15,23 +15,23 @@ namespace flex
 	}
 
 	Transform::Transform(const glm::vec3& position, const glm::quat& rotation, const glm::vec3& scale) :
-		position(position),
-		rotation(rotation),
-		scale(scale)
+		localPosition(position),
+		localRotation(rotation),
+		localScale(scale)
 	{
 	}
 
 	Transform::Transform(const glm::vec3& position, const glm::quat& rotation) :
-		position(position),
-		rotation(rotation),
-		scale(glm::vec3(1.0f))
+		localPosition(position),
+		localRotation(rotation),
+		localScale(glm::vec3(1.0f))
 	{
 	}
 
 	Transform::Transform(const glm::vec3& position) :
-		position(position),
-		rotation(glm::quat(glm::vec3(0.0f))),
-		scale(glm::vec3(1.0f))
+		localPosition(position),
+		localRotation(glm::quat(glm::vec3(0.0f))),
+		localScale(glm::vec3(1.0f))
 	{
 	}
 
@@ -41,37 +41,106 @@ namespace flex
 
 	void Transform::Translate(glm::vec3 deltaPosition)
 	{
-		position += deltaPosition;
+		localPosition += deltaPosition;
+
+		UpdateParentTransform();
+	}
+
+	void Transform::Translate(float deltaX, float deltaY, float deltaZ)
+	{
+		localPosition.x += deltaX;
+		localPosition.y += deltaY;
+		localPosition.z += deltaZ;
+
+		UpdateParentTransform();
 	}
 
 	void Transform::Rotate(glm::quat deltaQuatRotation)
 	{
-		rotation *= deltaQuatRotation;
+		localRotation *= deltaQuatRotation;
+
+		UpdateParentTransform();
 	}
 
 	void Transform::Rotate(glm::vec3 deltaEulerRotationRad)
 	{
 		glm::quat rotationQuat(deltaEulerRotationRad);
-		rotation *= rotationQuat;
+		localRotation *= rotationQuat;
+
+		UpdateParentTransform();
+	}
+
+	void Transform::Rotate(float deltaX, float deltaY, float deltaZ)
+	{
+		glm::quat rotationQuat(glm::vec3(deltaX, deltaY, deltaZ));
+		localRotation *= rotationQuat;
+
+		UpdateParentTransform();
 	}
 
 	void Transform::Scale(glm::vec3 deltaScale)
 	{
-		scale *= deltaScale;
+		localScale *= deltaScale;
+
+		UpdateParentTransform();
+	}
+
+	void Transform::Scale(float deltaX, float deltaY, float deltaZ)
+	{
+		localScale.x *= deltaX;
+		localScale.y *= deltaY;
+		localScale.z *= deltaZ;
+
+		UpdateParentTransform();
+	}
+
+	void Transform::SetParentTransform(Transform* parent)
+	{
+		parentTransform = parent;
+	}
+
+	void Transform::AddChildTransform(Transform* child)
+	{
+		childrenTransforms.push_back(child);
+
+		UpdateParentTransform();
+	}
+
+	void Transform::RemoveChildTransform(Transform* child)
+	{
+		for (auto iter = childrenTransforms.begin(); iter != childrenTransforms.end(); ++iter)
+		{
+			if (*iter == child)
+			{
+				childrenTransforms.erase(iter);
+				return;
+			}
+		}
+	}
+
+	void Transform::RemoveAllChildTransforms()
+	{
+		auto iter = childrenTransforms.begin();
+		while (iter != childrenTransforms.end())
+		{
+			iter = childrenTransforms.erase(iter);
+		}
 	}
 
 	void Transform::SetAsIdentity()
 	{
-		position = glm::vec3(0.0f);
-		rotation = glm::quat(glm::vec3(0.0f));
-		scale = glm::vec3(1.0f);
+		localPosition = glm::vec3(0.0f);
+		localRotation = glm::quat(glm::vec3(0.0f));
+		localScale = glm::vec3(1.0f);
+
+		UpdateParentTransform();
 	}
 
 	glm::mat4 Transform::GetModelMatrix()
 	{
-		glm::mat4 matTrans = glm::translate(glm::mat4(1.0f), position);
-		glm::mat4 matRot = glm::mat4(rotation);
-		glm::mat4 matScale = glm::scale(glm::mat4(1.0f), scale);
+		glm::mat4 matTrans = glm::translate(glm::mat4(1.0f), globalPosition);
+		glm::mat4 matRot = glm::mat4(globalRotation);
+		glm::mat4 matScale = glm::scale(glm::mat4(1.0f), globalScale);
 		glm::mat4 matModel = matTrans * matRot * matScale;
 
 		return matModel;
@@ -85,4 +154,35 @@ namespace flex
 
 		return result;
 	}
+
+	void Transform::UpdateParentTransform()
+	{
+		if (parentTransform)
+		{
+			parentTransform->UpdateParentTransform();
+		}
+		else
+		{
+			globalPosition = localPosition;
+			globalRotation = localRotation;
+			globalScale = localScale;
+
+			UpdateChildTransforms();
+		}
+	}
+
+	void Transform::UpdateChildTransforms()
+	{
+		for (auto iter = childrenTransforms.begin(); iter != childrenTransforms.end(); ++iter)
+		{
+			Transform* childTransform = *iter;
+
+			childTransform->globalPosition = childTransform->localPosition + localPosition;
+			childTransform->globalScale = childTransform->localScale * localScale;
+			childTransform->globalRotation = childTransform->localRotation * localRotation;
+
+			childTransform->UpdateChildTransforms();
+		}
+	}
+
 } // namespace flex
