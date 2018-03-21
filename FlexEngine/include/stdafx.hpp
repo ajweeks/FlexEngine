0fe5561b2ddb6381c7d3fc8609b760b6fad053f7
AJ Weeks <ajweeks@shaw.ca>
2018-03-21 08:35:27 +0000

    Disable warning diables

--- FlexEngine/include/stdafx.hpp
+++ FlexEngine/include/stdafx.hpp
@@ -5,10 +5,10 @@
 #define COMPILE_OPEN_GL 1
 #define COMPILE_VULKAN 1
 
-#pragma warning(disable : 4201)
-#pragma warning(disable : 4820)
-#pragma warning(disable : 4868)
-#pragma warning(disable : 4710)
+#pragma warning(disable : 4201) // nonstandard extension used: nanmeless struct/union
+//#pragma warning(disable : 4820) // bytes' bytes padding added after construct 'member_name'
+//#pragma warning(disable : 4868) // compiler may not enforce left-to-right evaluation order in braced initializer list
+//#pragma warning(disable : 4710) // function not inlined
 
 #include "Types.hpp"
 
