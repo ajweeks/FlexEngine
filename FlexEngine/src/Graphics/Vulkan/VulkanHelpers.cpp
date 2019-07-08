a22462205fb1084d55d482e0756f4f86eb35f234
AJ Weeks <ajweeks@shaw.ca>
2019-07-08 08:19:55 +0100

    Remove unused local from CreateAttachment

--- FlexEngine/src/Graphics/Vulkan/VulkanHelpers.cpp
+++ FlexEngine/src/Graphics/Vulkan/VulkanHelpers.cpp
@@ -1737,14 +1737,12 @@ namespace flex
 			FrameBufferAttachment *attachment)
 		{
 			VkImageAspectFlags aspectMask = 0;
-			VkImageLayout imageLayout;
 
 			attachment->format = format;
 
 			if (usage & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)
 			{
 				aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-				imageLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 			}
 			if (usage & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
 			{
@@ -1755,8 +1753,6 @@ namespace flex
 				{
 					aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
 				}
-
-				imageLayout = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_STENCIL_READ_ONLY_OPTIMAL;
 			}
 
 			assert(aspectMask > 0);
