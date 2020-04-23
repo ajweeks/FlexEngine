f51c066f637d48c34cd70fcf82014bdd07ec0090
AJ Weeks <ajweeks@shaw.ca>
2020-04-23 18:52:36 +0100

    Fix directory watch compilation on windows

--- FlexEngine/include/Platform/Platform.hpp
+++ FlexEngine/include/Platform/Platform.hpp
@@ -62,7 +62,7 @@ namespace flex
 		bool m_bInstalled = false;
 		bool m_bWatchSubtree = false;
 		std::string m_Directory;
-		u64 m_ChangeHandle;
+		void* m_ChangeHandle = nullptr;
 
 	};
 } // namespace flex
