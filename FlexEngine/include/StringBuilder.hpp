9b03e02737f017549c00509ae8ca711ab0b72d36
AJ Weeks <ajweeks@shaw.ca>
2020-07-20 20:06:01 +0100

    Add string builder

---
+++ FlexEngine/include/StringBuilder.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "JSONTypes.hpp"
+
+namespace flex
+{
+	// TODO: Test
+	class StringBuilder
+	{
+	public:
+		StringBuilder(u32 initialCapacity = 0);
+
+		void Append(const std::string& str);
+		void Append(const char* str);
+		void Append(char c);
+
+		void Clear();
+
+		std::string ToString();
+		// NOTE: Pointer should not be held onto! Unsafe once anything else is appended
+		const char* ToCString();
+		u32 Length() const;
+
+	private:
+		void Resize(u32 newSize);
+
+		std::vector<char> buffer;
+		u32 length;
+
+	};
+} // namespace flex
---
+++ FlexEngine/src/StringBuilder.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.hpp"
+
+#include "StringBuilder.hpp"
+
+namespace flex
+{
+	StringBuilder::StringBuilder(u32 initialCapacity) :
+		buffer(initialCapacity, '\0'),
+		length(0)
+	{
+	}
+
+	void StringBuilder::Append(const std::string& str)
+	{
+		Append(str.c_str());
+	}
+
+	void StringBuilder::Append(const char* str)
+	{
+		if ((length + (u32)strlen(str)) >= (u32)buffer.size())
+		{
+			Resize((length + (u32)strlen(str)) * 2);
+		}
+
+		char* c = (char*)str;
+		while (c != '\0')
+		{
+			buffer[length++] = *c++;
+		}
+	}
+
+	void StringBuilder::Append(char c)
+	{
+		if ((length + 1) >= (u32)buffer.size())
+		{
+			Resize((length + 1) * 2);
+		}
+
+		buffer[length++] = c;
+	}
+
+	void StringBuilder::Clear()
+	{
+		buffer.clear();
+		length = 0;
+	}
+
+	std::string StringBuilder::ToString()
+	{
+		std::string result(buffer.data(), length);
+		return result;
+	}
+
+	const char* StringBuilder::ToCString()
+	{
+		const char* result = buffer.data();
+		return result;
+	}
+
+	u32 StringBuilder::Length() const
+	{
+		return length;
+	}
+
+	void StringBuilder::Resize(u32 newSize)
+	{
+		buffer.resize(newSize, '\0');
+	}
+} // namespace flex
