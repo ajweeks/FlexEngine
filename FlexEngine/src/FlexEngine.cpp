7f80b96460228b25c8c0001f6e4d300710199512
AJ Weeks <ajweeks@shaw.ca>
2021-05-09 22:21:14 +0100

    Bump engine version to 0.8.7

--- FlexEngine/src/FlexEngine.cpp
+++ FlexEngine/src/FlexEngine.cpp
@@ -78,7 +78,7 @@ namespace flex
 
 	const u32 FlexEngine::EngineVersionMajor = 0;
 	const u32 FlexEngine::EngineVersionMinor = 8;
-	const u32 FlexEngine::EngineVersionPatch = 6;
+	const u32 FlexEngine::EngineVersionPatch = 7;
 
 	std::string FlexEngine::s_CurrentWorkingDirectory;
 	std::string FlexEngine::s_ExecutablePath;
