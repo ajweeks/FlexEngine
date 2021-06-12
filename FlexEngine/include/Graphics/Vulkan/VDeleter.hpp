ee3eed9b904a373f8bd2b6d34aa78e57ad2cff25
AJ Weeks <ajweeks@shaw.ca>
2021-06-12 11:18:34 +0100

    Clean up VDeleter

--- FlexEngine/include/Graphics/Vulkan/VDeleter.hpp
+++ FlexEngine/include/Graphics/Vulkan/VDeleter.hpp
@@ -12,85 +12,63 @@ namespace flex
 		class VDeleter
 		{
 		public:
-			VDeleter();
+			VDeleter() :
+				VDeleter([](T, VkAllocationCallbacks*) {})
+			{
+			}
 
-			VDeleter(std::function<void(T, VkAllocationCallbacks*)> deletef);
-			VDeleter(const VDeleter<VkDevice>& device, std::function<void(VkDevice, T, VkAllocationCallbacks*)> deletef);
-			~VDeleter();
+			VDeleter(std::function<void(T, VkAllocationCallbacks*)> deletef)
+			{
+				this->deleter = [=](T obj) { deletef(obj, nullptr); };
+			}
 
-			T* operator &();
-			T* replace();
-			operator T() const;
-			void operator=(T rhs);
+			VDeleter(const VDeleter<VkDevice>& device, std::function<void(VkDevice, T, VkAllocationCallbacks*)> deletef)
+			{
+				this->deleter = [&device, deletef](T obj) { deletef(device, obj, nullptr); };
+			}
+
+			~VDeleter()
+			{
+				cleanup();
+			}
+
+			T* operator &()
+			{
+				return &object;
+			}
+
+			T* replace()
+			{
+				cleanup();
+				return &object;
+			}
+
+			operator T() const
+			{
+				return object;
+			}
+
+			void operator=(T rhs)
+			{
+				if (rhs != object)
+				{
+					cleanup();
+					object = rhs;
+				}
+			}
 
 		private:
 			T object{ VK_NULL_HANDLE };
 			std::function<void(T)> deleter;
 
-			void cleanup();
+			void cleanup()
+			{
+				if (object != VK_NULL_HANDLE)
+				{
+					deleter(object);
+				}
+				object = VK_NULL_HANDLE;
+			}
 		};
-
-		template<typename T>
-		VDeleter<T>::VDeleter() :
-			VDeleter([](T, VkAllocationCallbacks*) {})
-		{
-		}
-
-		template<typename T>
-		VDeleter<T>::VDeleter(std::function<void(T, VkAllocationCallbacks*)> deletef)
-		{
-			this->deleter = [=](T obj) { deletef(obj, nullptr); };
-		}
-
-		template<typename T>
-		VDeleter<T>::VDeleter(const VDeleter<VkDevice>& device, std::function<void(VkDevice, T, VkAllocationCallbacks*)> deletef)
-		{
-			this->deleter = [&device, deletef](T obj) { deletef(device, obj, nullptr); };
-		}
-
-		template<typename T>
-		VDeleter<T>::~VDeleter()
-		{
-			cleanup();
-		}
-
-		template<typename T>
-		T* VDeleter<T>::operator &()
-		{
-			return &object;
-		}
-
-		template<typename T>
-		T* VDeleter<T>::replace()
-		{
-			cleanup();
-			return &object;
-		}
-
-		template<typename T>
-		VDeleter<T>::operator T() const
-		{
-			return object;
-		}
-
-		template<typename T>
-		void VDeleter<T>::operator=(T rhs)
-		{
-			if (rhs != object)
-			{
-				cleanup();
-				object = rhs;
-			}
-		}
-
-		template<typename T>
-		void VDeleter<T>::cleanup()
-		{
-			if (object != VK_NULL_HANDLE)
-			{
-				deleter(object);
-			}
-			object = VK_NULL_HANDLE;
-		}
 	} // namespace vk
 } // namespace flex
