d33672aad536b7808c2959ffe80dc177666630ac
AJ Weeks <ajweeks@shaw.ca>
2021-12-20 16:03:17 +0100

    Update Vulkan headers to 1.2.198.1

--- FlexEngine/src/Graphics/Vulkan/VulkanRenderer.cpp
+++ FlexEngine/src/Graphics/Vulkan/VulkanRenderer.cpp
@@ -5525,7 +5525,7 @@ namespace flex
 			createInfo.imageColorSpace = surfaceFormat.colorSpace;
 			createInfo.imageExtent = extent;
 			createInfo.imageArrayLayers = 1;
-			createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
+			createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 
 			VulkanQueueFamilyIndices indices = m_VulkanDevice->m_QueueFamilyIndices;
 			u32 queueFamilyIndices[] = { (u32)indices.graphicsFamily, (u32)indices.presentFamily };
