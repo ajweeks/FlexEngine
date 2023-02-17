f3157b995d5d0b7b01d8907405bc7fde36b8fe20
AJ Weeks <ajweeks@shaw.ca>
2023-02-17 09:59:42 +0100

    Bump number of descriptor sets

--- FlexEngine/include/Graphics/Vulkan/VulkanHelpers.hpp
+++ FlexEngine/include/Graphics/Vulkan/VulkanHelpers.hpp
@@ -687,7 +687,7 @@ namespace flex
 			VkDescriptorPool GetPool() const;
 
 			// TODO: Monitor number of used desc sets to set this value intelligently
-			u32 maxNumDescSets = 1024;
+			u32 maxNumDescSets = 4096;
 			static const u32 MAX_NUM_DESC_COMBINED_IMAGE_SAMPLERS = 8;
 			static const u32 MAX_NUM_DESC_UNIFORM_BUFFERS = 2;
 			static const u32 MAX_NUM_DESC_DYNAMIC_UNIFORM_BUFFERS = 2;
